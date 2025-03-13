2d51f4db0f3613e2efc823a827f2d90dbfc28735
umumararu <umumararu@alustudent.com>
2025-03-13 08:13:07 -0700

    SQL queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
--- show all privileges of data users 
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2.
+
+-- Show grants for user_0d_1 (root privileges)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2 (SELECT and INSERT on user_2_db)
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
